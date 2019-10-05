46159b6c888302bc550f3dd30db270265dce664a
WILLIAM ALEXANDER MORENO PRIETO <williammoreno@javeriana.edu.co>
2019-10-05 12:36:17 -0500

    Update ServicioPaseo.java

--- Back/PaseosServerWS/src/main/java/co/edu/javeriana/webservice/service/ServicioPaseo.java
+++ Back/PaseosServerWS/src/main/java/co/edu/javeriana/webservice/service/ServicioPaseo.java
@@ -5,10 +5,13 @@ import java.util.List;
 import javax.jws.WebService;
 
 import co.edu.javeriana.webservice.entities.*;
+import co.edu.javeriana.webservice.interfaceservice.InterfaceComentario;
+import co.edu.javeriana.webservice.interfaceservice.InterfacePregunta;
 import co.edu.javeriana.webservice.interfaceservice.InterfaceService;
+import co.edu.javeriana.webservice.interfaceservice.InterfaceUser;
 
 @WebService(endpointInterface = "co.edu.javeriana.webservice.interfaceservice.InterfaceService")
-public class ServicioPaseo implements InterfaceService {
+public class ServicioPaseo implements InterfaceService, InterfaceUser, InterfaceComentario, InterfacePregunta {
 
 	@Override
 	public Servicio crearServicio(Servicio paseo) {
@@ -36,6 +39,96 @@ public class ServicioPaseo implements InterfaceService {
 
 	@Override
 	public boolean eliminarPaseo(Long id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Pregunta crearPregunta(Pregunta comentario) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Pregunta leerPregunta(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Pregunta> leerTodosPreguntas(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Pregunta actualizarPregunta(Long id, Pregunta pregunta) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean eliminarPregunta(Long id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Comentario crearComentario(Comentario comentario) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Comentario leerComentario(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Comentario> leerTodosComentarios(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Comentario actualizarComentario(Long id, Comentario comentario) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean eliminarComentario(Long id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Usuario crearUsuario(Usuario paseo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Usuario leerUsuario(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Usuario> leerTodosUsuarios(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Usuario actualizarUsuario(Long id, Usuario usuario) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean eliminarUsuario(Long id) {
 		// TODO Auto-generated method stub
 		return false;
 	}
