9f9d28b355fc2fdac36822b7dfd62c967c9e6c52
Johan Murillo <johan-smc@hotmail.com>
2019-10-06 16:02:51 -0500

    create service

--- FrontEnd/frontecotech/src/app/app.module.ts
+++ FrontEnd/frontecotech/src/app/app.module.ts
@@ -11,6 +11,7 @@ import { ServicesListComponent } from './modules/services-list/services-list.com
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { SignupComponent } from './modules/signup/signup.component';
 import { DetailsComponent } from './modules/details/details.component';
+import { CreateServiceComponent } from './modules/create-service/create-service.component';
 
 
 @NgModule({
@@ -19,7 +20,8 @@ import { DetailsComponent } from './modules/details/details.component';
     LoginComponent,
     SignupComponent,
     ServicesListComponent,
-    DetailsComponent
+    DetailsComponent,
+    CreateServiceComponent
   ],
   imports: [
     BrowserModule,
