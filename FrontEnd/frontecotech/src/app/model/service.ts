d1b6e3d3c8ef710afffe6ab1bbf77620f8bb77bc
WILLIAM ALEXANDER MORENO PRIETO <williammoreno@javeriana.edu.co>
2019-10-06 15:00:59 -0500

    model ts

--- FrontEnd/frontecotech/src/app/model/service.ts
+++ FrontEnd/frontecotech/src/app/model/service.ts
@@ -9,18 +9,31 @@ export class Service {
     id: string;
 }
 
-export class Paseo {
-    contacto: string;
-    costo: number;
-    descripcion: string;
-    latitud: number;
-    longitud: number;
-    nombre: string;
-    tipo: string;
-    id: string;
+export class Paseo extends Service{
     horaSalida: string;
     horaLlegada: string;
     trayecto: string;
     restricciones: string;
     sugerencias: string;
+}
+
+export class Alimentacion extends Service {
+    tipoComida: string;
+}
+
+export class Alojamiento extends Service {
+    numHabaitaciones: number;
+    numHuespedes:  number;
+    extras: String;
+}
+
+export class Otro extends Service {
+    tioServicio: String;
+
+}
+
+export class Trasporte extends Service {
+
+    horaSalida: string;
+    horaLlegada: string;
 }
