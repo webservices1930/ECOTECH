d1304e14ca3627bf6a2eb022efcdfd352b46460a
William Baquero <williambaqueror@gmail.com>
2019-10-06 19:31:00 -0500

    UploadServices

--- FrontEnd/frontecotech/src/app/modules/shop-car/shop-car.component.ts
+++ FrontEnd/frontecotech/src/app/modules/shop-car/shop-car.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Service } from 'src/app/model/service';
+import { ServiceService } from 'src/app/services/service.service';
+import { SoapService } from 'src/app/services/soap.service';
+import { Client } from 'ngx-soap';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-shop-car',
@@ -7,9 +13,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShopCarComponent implements OnInit {
 
-  constructor() { }
+  servicios: Service[];
+  serviciosTemp: Service[];
+
+  constructor(
+    private serviceService: ServiceService,
+    private formBuilder: FormBuilder,
+    private soapService: SoapService,
+    private cookieService: CookieService) { }
 
   ngOnInit() {
+    this.soapService.client.then(client => {
+      this.serviceService.getAllServices(client as Client).subscribe(res => {
+        console.log('Services enviado');
+        console.log(res);
+        this.servicios = res.result.return;
+        console.log('servicios');
+        console.log(this.servicios);
+      });
+    });
+    this.cargarServicios();
   }
+
+  cargarServicios(){
+    var numServicios = Number(this.cookieService.get("count"));
+    for(var i=0;i<10){
+
+    }
+  }  
 
 }
