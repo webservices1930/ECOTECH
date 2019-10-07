94c0b8ee1ef7c12177905d49e47bdabd33bc5f22
William Baquero <williambaqueror@gmail.com>
2019-10-07 00:23:16 -0500

    counterItems

--- FrontEnd/frontecotech/src/app/modules/navbar/navbar.component.ts
+++ FrontEnd/frontecotech/src/app/modules/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
+import { count } from 'rxjs/operators';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  carrito:string;
+
+  constructor(private cookieService: CookieService) { }
 
   ngOnInit() {
+    this.carrito = this.cookieService.get("count");
   }
 
 }
