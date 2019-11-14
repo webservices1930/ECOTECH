406435ade5f6cf8715a356d94edafa4f08cd387e
William Moreno <williammoreno@javeriana.edu.co>
2019-11-14 11:46:41 -0500

    init spring project

---
+++ rest-backend/ecotech/src/main/java/com/javeriana/ecotech/EcotechApplication.java
@@ -0,0 +1,13 @@
+package com.javeriana.ecotech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcotechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcotechApplication.class, args);
+	}
+
+}
